5a51c5d5172578e1a84af1617cf81a921ce6e462
khorjingqian <jingqian1998@gmail.com>
2020-07-21 15:58:24 +0800

    Updates

---
+++ precache-manifest.34908ace8171cb21ae638bf5a7983558.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bcbabd37acfce06473913578d438d0d",
+    "url": "/covid-19-map/index.html"
+  },
+  {
+    "revision": "0d8c761064fed9039382",
+    "url": "/covid-19-map/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "64dc812589be303b39ed",
+    "url": "/covid-19-map/static/js/2.6db4ea97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-map/static/js/2.6db4ea97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d8c761064fed9039382",
+    "url": "/covid-19-map/static/js/main.c9438ed1.chunk.js"
+  },
+  {
+    "revision": "cd9453847678d7db8f81",
+    "url": "/covid-19-map/static/js/runtime-main.cd4e08b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19-map/static/media/logo.5d5d9eef.svg"
+  }
+]);
